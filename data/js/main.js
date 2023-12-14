d62086f9b381d8f3b1808e0fbcb7b1b5c9da3f4c
treviasxk <trevias@live.com>
2023-12-14 17:23:39 -0300

    • new system page

---
+++ data/js/main.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+    includeHTML();
+}
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    var post = false;
+    for(i = 0; i < z.length; i++){
+      elmnt = z[i];
+      file = elmnt.getAttribute("w3-include-html");
+      if(file == "data/pages/home.html"){
+        const urlParams = new URLSearchParams(window.location.search);
+        let searchParams = new URLSearchParams(urlParams);
+        if(searchParams.has("post")){
+            post = true;
+            file = "data/pages/posts/" + searchParams.get("post") + ".html";
+        }
+      }
+      if(file){
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+                elmnt.innerHTML = this.responseText;
+                if(post){
+                    elmnt.innerHTML += '<p class="date">Last Modified: '+ document.lastModified +'</p>';
+                }
+            }
+            if (this.status == 404) {elmnt.innerHTML = "<h1>Page not found.</h1>";}
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+}
