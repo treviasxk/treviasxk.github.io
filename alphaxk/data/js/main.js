0e2450573b954de5b44c31914813c0599aa2e610
treviasxk <trevias@live.com>
2023-12-15 00:24:42 -0300

    • new fixed

--- alphaxk/data/js/main.js
+++ alphaxk/data/js/main.js
@@ -1,50 +1,32 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-import { getDatabase, ref, child, get, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyArxSt_UwqzzkVeEf9ANDEcDypTKEmpHDI",
-    authDomain: "treviasxk-7bcea.firebaseapp.com",
-    databaseURL: "https://treviasxk-7bcea-default-rtdb.firebaseio.com",
-    projectId: "treviasxk-7bcea",
-    storageBucket: "treviasxk-7bcea.appspot.com",
-    messagingSenderId: "57991335902",
-    appId: "1:57991335902:web:ea336e43d3eeffeeb3574f",
-    measurementId: "G-NHNG5YZC8S"
-};
+const supabaseUrl = "https://dfmkiesijkrlfgqovkgy.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmbWtpZXNpamtybGZncW92a2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI2MDQzODgsImV4cCI6MjAxODE4MDM4OH0.hfyPYOMyU7udufL9syI17e8VOGs7swbfe4gPE54aWqI"
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 window.onload = function(){
   includeHTML();
 }
 
 
-function writeUserData(email) {
-  const db = getDatabase();
-  set(ref(db, 'emails/email'), {
-    emails: email,
-  });
-}
-
-
 function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  z = document.getElementsByTagName("*");
+  var z, i, elmnt, file;
+  z = document.getElementsByTagName("div");
   for(i = 0; i < z.length; i++){
     elmnt = z[i];
     file = elmnt.getAttribute("w3-include-html");
 
     const urlParams = new URLSearchParams(window.location.search);
     let searchParams = new URLSearchParams(urlParams);
-    if(searchParams.has("post") && file == "data/pages/home.html"){
+
+    if(searchParams.get("post") && file == "data/pages/home.html"){
       LoadPost(searchParams.get("post"), elmnt);
     }else{
-      if(file){
-        xhttp = new XMLHttpRequest();
+    if(file){
+        var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){
           if (this.readyState == 4) {
-            if (this.status == 200) {
-                elmnt.innerHTML = this.responseText;
-            }
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
             if (this.status == 404) {elmnt.innerHTML = "<h1>Page not found.</h1>";}
             elmnt.removeAttribute("w3-include-html");
             includeHTML();
@@ -58,19 +40,18 @@ function includeHTML() {
   }
 }
 
-function LoadPost(id, content){
-  initializeApp(firebaseConfig);
-  const dbRef = ref(getDatabase());
-  get(child(dbRef, `alphaxk/posts/` + id)).then((snapshot) => {
-  if(snapshot.exists()) {
-      document.title = snapshot.val().title + " - " + document.title;
-      content.innerHTML = "<h2>" + snapshot.val().title + "</h2><hr/>";
-      content.innerHTML += snapshot.val().content;
-      content.innerHTML += '<p class="date">Date: '+snapshot.val().date+'</p>'
-  } else {
-      content.innerHTML = "<h1>Page not found.</h1>";
+async function LoadPost(id, content){
+  content.removeAttribute("w3-include-html");
+  const { data } = await supabase
+  .from('posts')
+  .select()
+  .eq('id', id);
+  if(data[0]){
+    document.title = data[0].title + " - " + document.title;
+    content.innerHTML = "<h2>" + data[0].title + "</h2><hr/>";
+    content.innerHTML += data[0].content;
+    content.innerHTML += '<p class="date">Date: '+ data[0].date +'</p>'
+  }else{
+    content.innerHTML = "<h1>Page not found.</h1>";
   }
-  }).catch((error) => {
-    console.error(error);
-  });
 }
