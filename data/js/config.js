7e72c7bca09e4853e6789e0572b5204d4c295589
treviasxk <trevias@live.com>
2025-02-25 16:09:46 -0300

    Restored project.

--- data/js/config.js
+++ data/js/config.js
@@ -2,14 +2,17 @@
     Software Developed by Trevias Xk
     Social Networks:     treviasxk
     Github:              https://github.com/treviasxk
-    Paypal:              trevias@live.com
 */
 
-// Page Config
-title = "Trevias Xk";
-github = "treviasxk";
-instagram = "treviasxk";
-x = "treviasxk";
-linkedin = "treviasxk";
-totalPosts = 20;
-maxCharPosts = 500;
+// Settings
+var Title = "Trevias Xk";
+var Github = "#";
+var Instagram = "#";
+var X = "#";
+var Linkedin = "#";
+var TotalPostsLoadInScroll = 20;
+var MaxCharacteresPosts = 500;
+
+// Supabase Config
+var SupabaseUrl = "https://cyhgmlceqsfktdivwbgg.supabase.co";
+var SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5aGdtbGNlcXNma3RkaXZ3YmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2Mjc0NDUsImV4cCI6MjA1NTIwMzQ0NX0.LLExdL6Q0n87rzG8e6FAoNw0vhhoZTPraWIrN3jnRgk";
--- data/js/supabase.js
+++ data/js/supabase.js
@@ -2,12 +2,13 @@
     Software Developed by Trevias Xk
     Social Networks:     treviasxk
     Github:              https://github.com/treviasxk
-    Paypal:              trevias@live.com
 */
 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-// Supabase Config
-const supabaseUrl = "https://cyhgmlceqsfktdivwbgg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5aGdtbGNlcXNma3RkaXZ3YmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2Mjc0NDUsImV4cCI6MjA1NTIwMzQ0NX0.LLExdL6Q0n87rzG8e6FAoNw0vhhoZTPraWIrN3jnRgk";
-supabase = createClient(supabaseUrl, supabaseKey)
+try{
+    supabase = createClient(SupabaseUrl, SupabaseKey)
+}catch(e){
+    if(searchParams.get("page") == "blog")
+        ShowToast(e);
+}
